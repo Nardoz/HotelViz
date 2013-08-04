80b2c65b40e1749b7f0ec0f62e927dbfd55f5b2d
opensas <opensas@gmail.com>
2013-08-03 23:49:34 -0300

    fixes #1

--- js/main.js
+++ js/main.js
@@ -1,205 +1,220 @@
+/*globals window, document, DatasetFilter*/
+
 var DatavizTurismo;
 
 ;(function(global, document, $, d3, ko){
 
-    "use strict";
+  'use strict';
 
-    DatavizTurismo = global.DatavizTurismo = global.DatavizTurismo || {};
+  DatavizTurismo = global.DatavizTurismo = global.DatavizTurismo || {};
 
-	DatavizTurismo.$slider = $('#slider');
+  DatavizTurismo.$slider = $('#slider');
 
-    DatavizTurismo.sliderOptions = {
-        min:0,
-        max:100,
-        step:1,
-        orientation:'vertical',
-        tooltip:'show',
-        handle:'round',
-        selection:'after',
-        formater:function(v){
-            return DatavizTurismo.convertSliderValue(v)+'%';
-        }
-    }; 
-
-    DatavizTurismo.headers = [];
-
-    DatavizTurismo.data = [];
-
-    DatavizTurismo.map;
-
-    DatavizTurismo.$twitterButton = $('.twitter');
-
-    DatavizTurismo.$facebookButton = $('.facebook');
-
-    DatavizTurismo.$googleButton = $('.gplus');
-
-    DatavizTurismo.$text = $('.texto-resumen');
-
-    DatavizTurismo.$orderSelectors = $('.filter-order');
-
-    DatavizTurismo.$consultarBtn = $('#consultar');
-
-    DatavizTurismo.$desdeBtn = $('#fecha-desde');
-
-    DatavizTurismo.$hastaBtn = $('#fecha-hasta');
-
-    DatavizTurismo.$filter = $('#filter');
-
-    DatavizTurismo.$fullScreenBtb = $('#full-screen-btn');
-
-    DatavizTurismo.filter = new DatasetFilter;
-
-    DatavizTurismo.bindings = {
-        percentage:ko.observable(0),
-        percentageTotal: 0,
-        poblacionTotal:ko.observable(0),
-        superficieTotal:ko.observable(0),
-        cantSelected:ko.observable(0),
-        supSelected:ko.observable(0),
-        percentageSupSelected:0,
-        superficieTotalStr:0,
-        poblacionTotalStr:0,
-        availableFilters:ko.observable(),
-        selectedFilter:ko.observable(),
-        selectedOrder:ko.observable('DESCENDENTE')
-    };
-
-    var FilterOption = function(name, id, icon) {
-        this.name = name;
-        this.id = id;
-        this.icon = icon;
-    };
-
-    DatavizTurismo.init = function () {
-        //Init map
-        DatavizTurismo.map = d3.datavizTurismo('map-container',$('#map-container').width());
-
-        //Parsing Data
-        DatavizTurismo.retrieveData();
-
-        //Init button
-        DatavizTurismo.$consultarBtn.on('click',DatavizTurismo.filterData);
-        DatavizTurismo.$twitterButton.on('click',DatavizTurismo.shareTwitter);
-        DatavizTurismo.$facebookButton.on('click',DatavizTurismo.shareFacebook);
-        DatavizTurismo.$googleButton.on('click',DatavizTurismo.shareGoogle);
-        DatavizTurismo.$fullScreenBtb.on('click',DatavizTurismo.fullScreen);
-    };
-
-    DatavizTurismo.retrieveData = function(){
-
-        $.getJSON('data/ocupacion_hotelera.json', function(j){
-            DatavizTurismo.data = j.rows;
-            DatavizTurismo.filter.setDataset(j.rows);
-            DatavizTurismo.filterData();
-        });
-        
-    };
-
-    DatavizTurismo.filterData = function(){
-        var j = DatavizTurismo.filter.filter(DatavizTurismo.$desdeBtn.val(),DatavizTurismo.$hastaBtn.val());
-        DatavizTurismo.updateMap(j);
-    };
-
-    DatavizTurismo.fullScreen = function() {
-        var el = document.documentElement
-        , rfs =
-                el.requestFullScreen
-                || el.webkitRequestFullScreen
-                || el.mozRequestFullScreen
-        ;
-        rfs.call(el);
-    };
-
-    DatavizTurismo.getLocation = function(href) {
-        var l = document.createElement("a");
-        l.href = href;
-        return l;
-    };
-
-    DatavizTurismo.shareTwitter = function(e){
-        e.preventDefault();
-        var qObj = {
-            'text': DatavizTurismo.$text.text(),
-            'related': 'palamago,lndata',
-            'hashtags': 'argentina,censo,paisFederal'
-        };
-
-        var qs = $.param(qObj);
-
-        var width  = 575,
-            height = 400,
-            left   = ($(window).width()  - width)  / 2,
-            top    = ($(window).height() - height) / 2,
-            url    = this.href+"?"+qs,
-            opts   = 'status=1' +
-                     ',width='  + width  +
-                     ',height=' + height +
-                     ',top='    + top    +
-                     ',left='   + left;
-        
-        window.open(url, 'Twitter', opts);
-     
-        return false;
+  DatavizTurismo.sliderOptions = {
+    min:0,
+    max:100,
+    step:1,
+    orientation:'vertical',
+    tooltip:'show',
+    handle:'round',
+    selection:'after',
+    formater:function(v){
+      return DatavizTurismo.convertSliderValue(v)+'%';
     }
+  };
 
-    DatavizTurismo.shareFacebook = function(e){
-        e.preventDefault();
-        var qs = 
-            '&p[url]='+window.location+
-            '&p[title]='+'Argentina, un país POCO federal...'+
-            '&p[images][0]='+window.location+'img/share.png'+
-            '&p[summary]='+DatavizTurismo.$text.text();
+  DatavizTurismo.headers = [];
 
-        var width  = 575,
-            height = 400,
-            left   = ($(window).width()  - width)  / 2,
-            top    = ($(window).height() - height) / 2,
-            url    = this.href+qs,
-            opts   = 'status=1' +
-                     ',width='  + width  +
-                     ',height=' + height +
-                     ',top='    + top    +
-                     ',left='   + left;
-        
-        window.open(url, 'Facebook', opts);
-     
-        return false;
-    }
+  DatavizTurismo.data = [];
 
-    DatavizTurismo.shareGoogle = function(e){
-        e.preventDefault();
-        var qs = 
-            'url='+ window.location;
+  DatavizTurismo.map;
 
-        var width  = 575,
-            height = 400,
-            left   = ($(window).width()  - width)  / 2,
-            top    = ($(window).height() - height) / 2,
-            url    = this.href+"?"+qs,
-            opts   = 'status=1' +
-                     ',width='  + width  +
-                     ',height=' + height +
-                     ',top='    + top    +
-                     ',left='   + left;
-        
-        window.open(url, 'Google+', opts);
-     
-        return false;
-    }
+  DatavizTurismo.rankingLimit = 3;
 
-    DatavizTurismo.updateMap = function (ciudades) {
-        DatavizTurismo.map.update(ciudades,DatavizTurismo.$filter.val());
+  DatavizTurismo.$twitterButton = $('.twitter');
+
+  DatavizTurismo.$facebookButton = $('.facebook');
+
+  DatavizTurismo.$googleButton = $('.gplus');
+
+  DatavizTurismo.$text = $('.texto-resumen');
+
+  DatavizTurismo.$orderSelectors = $('.filter-order');
+
+  DatavizTurismo.$consultarBtn = $('#consultar');
+
+  DatavizTurismo.$desdeBtn = $('#fecha-desde');
+
+  DatavizTurismo.$hastaBtn = $('#fecha-hasta');
+
+  DatavizTurismo.$filter = $('#filter');
+
+  DatavizTurismo.$fullScreenBtb = $('#full-screen-btn');
+
+  DatavizTurismo.filter = new DatasetFilter();
+
+  DatavizTurismo.bindings = {
+    percentage:ko.observable(0),
+    percentageTotal: 0,
+    poblacionTotal:ko.observable(0),
+    superficieTotal:ko.observable(0),
+    cantSelected:ko.observable(0),
+    supSelected:ko.observable(0),
+    percentageSupSelected:0,
+    superficieTotalStr:0,
+    poblacionTotalStr:0,
+    availableFilters:ko.observable(),
+    selectedFilter:ko.observable(),
+    selectedOrder:ko.observable('DESCENDENTE')
+  };
+
+  var FilterOption = function(name, id, icon) {
+    this.name = name;
+    this.id = id;
+    this.icon = icon;
+  };
+
+  DatavizTurismo.init = function () {
+    //Init map
+    DatavizTurismo.map = d3.datavizTurismo('map-container',$('#map-container').width());
+
+    //Parsing Data
+    DatavizTurismo.retrieveData();
+
+    //Init button
+    DatavizTurismo.$consultarBtn.on('click',DatavizTurismo.filterData);
+    DatavizTurismo.$twitterButton.on('click',DatavizTurismo.shareTwitter);
+    DatavizTurismo.$facebookButton.on('click',DatavizTurismo.shareFacebook);
+    DatavizTurismo.$googleButton.on('click',DatavizTurismo.shareGoogle);
+    DatavizTurismo.$fullScreenBtb.on('click',DatavizTurismo.fullScreen);
+  };
+
+  DatavizTurismo.retrieveData = function(){
+
+    $.getJSON('data/ocupacion_hotelera.json', function(j){
+      DatavizTurismo.data = j.rows;
+      DatavizTurismo.filter.setDataset(j.rows);
+      DatavizTurismo.filterData();
+    });
+
+  };
+
+  DatavizTurismo.filterData = function(){
+    var filter = DatavizTurismo.filter;
+    var j = filter.filter(
+      DatavizTurismo.$desdeBtn.val(),
+      DatavizTurismo.$hastaBtn.val()
+    );
+
+    var filterField = DatavizTurismo.$filter.val(),
+        limit = DatavizTurismo.rankingLimit;
+
+    DatavizTurismo.topRanking = filter.ranking(j, filterField, 'desc', limit);
+    DatavizTurismo.botttomRanking = filter.ranking(j, filterField, 'asc', limit);
+
+    DatavizTurismo.updateMap(j);
+  };
+
+  DatavizTurismo.fullScreen = function() {
+    var el = document.documentElement,
+        rfs =
+          el.requestFullScreen ||
+          el.webkitRequestFullScreen ||
+          el.mozRequestFullScreen
+    ;
+    rfs.call(el);
+  };
+
+  DatavizTurismo.getLocation = function(href) {
+    var l = document.createElement('a');
+    l.href = href;
+    return l;
+  };
+
+  DatavizTurismo.shareTwitter = function(e){
+    e.preventDefault();
+    var qObj = {
+      'text': DatavizTurismo.$text.text(),
+      'related': 'palamago,lndata',
+      'hashtags': 'argentina,censo,paisFederal'
     };
 
-    DatavizTurismo.dotSeparateNumber = function(val){
-        while (/(\d+)(\d{3})/.test(val.toString())){
-          val = val.toString().replace(/(\d+)(\d{3})/, '$1'+'.'+'$2');
-        }
-        return val;
-    };
+    var qs = $.param(qObj);
 
-})(window, document,jQuery, d3, ko);
+    var width  = 575,
+      height = 400,
+      left   = ($(window).width()  - width)  / 2,
+      top    = ($(window).height() - height) / 2,
+      url    = this.href + '?' + qs,
+      opts   = 'status=1' +
+           ',width='  + width  +
+           ',height=' + height +
+           ',top='    + top    +
+           ',left='   + left;
+
+    window.open(url, 'Twitter', opts);
+
+    return false;
+  };
+
+  DatavizTurismo.shareFacebook = function(e){
+    e.preventDefault();
+    var qs =
+      '&p[url]='+window.location+
+      '&p[title]='+'Argentina, un país POCO federal...'+
+      '&p[images][0]='+window.location+'img/share.png'+
+      '&p[summary]='+DatavizTurismo.$text.text();
+
+    var width  = 575,
+      height = 400,
+      left   = ($(window).width()  - width)  / 2,
+      top    = ($(window).height() - height) / 2,
+      url    = this.href+qs,
+      opts   = 'status=1' +
+           ',width='  + width  +
+           ',height=' + height +
+           ',top='    + top    +
+           ',left='   + left;
+
+    window.open(url, 'Facebook', opts);
+
+    return false;
+  };
+
+  DatavizTurismo.shareGoogle = function(e){
+    e.preventDefault();
+    var qs =
+      'url=' + window.location;
+
+    var width  = 575,
+      height = 400,
+      left   = ($(window).width()  - width)  / 2,
+      top    = ($(window).height() - height) / 2,
+      url    = this.href + '?' + qs,
+      opts   = 'status=1' +
+           ',width='  + width  +
+           ',height=' + height +
+           ',top='    + top    +
+           ',left='   + left;
+
+    window.open(url, 'Google+', opts);
+
+    return false;
+  };
+
+  DatavizTurismo.updateMap = function (ciudades) {
+    DatavizTurismo.map.update(ciudades,DatavizTurismo.$filter.val());
+  };
+
+  DatavizTurismo.dotSeparateNumber = function(val){
+    while (/(\d+)(\d{3})/.test(val.toString())){
+      val = val.toString().replace(/(\d+)(\d{3})/, '$1'+'.'+'$2');
+    }
+    return val;
+  };
+
+})(window, document, jQuery, d3, ko);
 
 window.onload = function() {
-    DatavizTurismo.init(); 
-}
+  DatavizTurismo.init();
+};
