9bfe0d2eb0b4f4d6d479dcbb8c38563f71cfd660
Pala <paladinop@gmail.com>
2013-08-03 22:29:24 -0300

    initial commit

---
+++ js/d3.datavizTurismo.js
@@ -0,0 +1,178 @@
+d3.datavizTurismo = function(containerId,width) {
+
+  //Init vars
+  var height=700,
+    centered,
+    projection,
+    path,
+    mapa_svg,
+    mainGroup,
+    departamentos,
+    provincias,
+    legend,
+    gran_buenos_aires,
+    gran_buenos_aires_mesh,
+    svg,
+    mini_svg,
+    AMBA_IDS = ["D02003", "D02004", "D02011", "D02017", "D02035", "D02036", "D02132", "D02038", "D02051", "D02052", "D02134", "D02133", "D02129", "D02061", "D02063", "D02062", "D02070", "D02130", "D02075", "D02077", "D02079", "D02080", "D02089", "D02128", "D02092", "D02105", "D02106", "D02053", "D02109", "D02113", "D02118", "D02122", "CAPFED"],
+    scale,
+    tooltip,
+    ciudades;
+  
+  function _init() {
+    _createMap();
+    _createTooltip();
+    _createPath();
+  };
+
+  function _createTooltip() {
+    //Crea el tooltip            
+    tooltip = d3.select("body").append("div")   
+                .attr("id", "tooltip")               
+                .style("opacity", 0);
+
+    svg.on("mousemove", mousemove);
+
+    function mousemove() {
+      tooltip.style("left", (d3.event.pageX + 20) + "px").style("top", (d3.event.pageY - 30) + "px");
+    }
+  }
+
+  function _createMap() {
+
+    svg = d3.select('#'+containerId).append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("class", "poblacion");
+
+  };
+
+  function _getName(e) {
+    return e.replace(/\s+/g, "-").toLowerCase()
+  };
+
+  function _createPath() {
+    scale = d3.geo.mercator().scale(900).center([-65, -34]).translate([width / 2 - 30, height / 2 - 125]);
+    projection = scale;
+    path = d3.geo.path().projection(scale);
+
+    d3.json(window.location.pathname+"data/argentina.json", function(error, e) {
+
+        //mapa
+        mapa_svg = svg.append("g").classed("mapa", !0).attr("transform", "translate(0, 0)");
+        
+        departamentos = mapa_svg.append("g").attr("class", "departamentos");
+        provincias = mapa_svg.append("g").attr("class", "provincias");
+        legend = svg.append("g").attr("class", "legend");
+
+        ciudades = svg.append("g").attr("class", "ciudades");
+        
+        var featuresProvincias = topojson.feature(e, e.objects.provincias).features,
+            featuresDepartamentos = topojson.feature(e, e.objects.departamentos).features;
+        
+        provincias.selectAll("path")
+          .data(featuresProvincias)
+          .enter()
+          .append("path")
+          .attr("id", function (e) {
+              return _getName(e.properties.PROVINCIA)
+          })
+          .attr("d", path)
+          .attr("class", "provincia");
+  
+
+        gran_buenos_aires = departamentos.append("g")
+          .attr("class", "gran-buenos-aires");
+
+        gran_buenos_aires_mesh = topojson.mesh(e, {
+            type: "GeometryCollection",
+            geometries: e.objects.departamentos.geometries.filter(function (e) {
+                return AMBA_IDS.indexOf(e.id) !== -1
+            })
+        });
+
+        featuresProvincias.forEach(function (e) {
+            var r = _getName(e.properties.PROVINCIA);
+            departamentos.append("g")
+            .attr("id", "provincia-" + r)
+              .selectAll("path")
+              .data(featuresDepartamentos.filter(function (e) {
+                return r === _getName(e.properties.p_id) && AMBA_IDS.indexOf(e.id) == -1;
+              }))
+              .enter()
+              .append("path")
+              .attr("id", function (e) {
+                  return e.id;
+              })
+              .attr("d", path)
+              .attr("class", "departamento")
+        });
+
+        departamentos.select("g#provincia-buenos-aires")
+          .append("g")
+          .attr("id", "gran-buenos-aires")
+          .selectAll("path")
+          .data(featuresDepartamentos.filter(function (e) {
+              return AMBA_IDS.indexOf(e.id) !== -1
+          }))
+          .enter()
+          .append("path")
+          .attr("id", function (e) {
+              return e.id
+          })
+          .attr("d", path)
+          .attr("class", "departamento");
+
+         //Tooltip
+        var m = mapa_svg.selectAll("path.departamento");
+ 
+        function addTooltipListener(s) {
+          s.on("mouseover", function(d) {
+              var innerHTML = d.properties.a + '<br/><strong>' + d.properties.p + '</strong>';        
+              tooltip.transition()        
+                     .duration(100)      
+                     .style("opacity", .9)
+
+              tooltip.html(innerHTML);
+              $(this)[0].classList.add("hover");
+          })
+          .on("mouseout", function(d) {
+              $(this)[0].classList.remove("hover");
+              tooltip.transition()        
+                      .duration(200)      
+                      .style("opacity", 0);   
+          });
+        };
+
+        addTooltipListener(m);
+
+    });
+
+  };
+
+  _init();
+
+  return {
+    
+    update: function(ciudades,field){
+
+      var circulos = svg.selectAll('circle.ciudad')
+      .data(ciudades)
+      .enter()
+      .append("circle")
+      .attr("class", "ciudad")
+      .attr("transform", function(d) {
+        return "translate(" + projection([d.lon,d.lat]) + ")"; 
+      });
+
+      svg.selectAll('circle.ciudad')
+      .transition(1000)
+      .attr("r",function(d){
+        return d[field]/100;
+      });
+
+    }
+
+  }
+
+}
---
+++ js/filter.js
@@ -0,0 +1,82 @@
+
+
+var DatasetFilter = function() {
+
+  var dataset = {};
+
+  this.setDataset = function(d) {
+    dataset = d;
+  }
+
+  this.filter = function(dateFrom, dateTo) {
+
+    if(dateFrom.split('-').length != 2) {
+      console.error('Invalid date start: ' + dateFrom);
+      return false;
+    }
+
+    if(dateTo.split('-').length != 2) {
+      console.error('Invalid date end: ' + dateTo);
+      return false;
+    }
+
+    var monthFrom = dateFrom.split('-')[0];
+    var yearFrom  = dateFrom.split('-')[1];
+
+    var monthTo = dateTo.split('-')[0];
+    var yearTo  = dateTo.split('-')[1];
+
+    var filtered = _.filter(dataset, function(item) {
+      return (item.anio >= yearFrom && item.mes >= monthFrom) && (item.anio <= yearTo && item.mes <= monthTo);
+    });
+
+    var groupedByCity = _.groupBy(filtered, function(item) {
+      return item.ciudad;
+    });
+
+    var data = [];
+
+    _.each(groupedByCity, function(city) {
+
+      var reduced = _.reduce(city, function(memo, item) {
+
+        var memo2 = _.clone(memo);
+
+        memo2.habitaciones_disp = memo2.habitaciones_disp + item.habitaciones_disp;
+        memo2.habitaciones_ocup = memo2.habitaciones_ocup + item.habitaciones_ocup;
+        memo2.plazas_disp       = memo2.plazas_disp + item.plazas_disp;
+        memo2.plazas_ocup       = memo2.plazas_ocup + item.plazas_ocup;
+        memo2.establecimientos  = memo2.establecimientos + item.establecimientos;
+        memo2.viajeros          = memo2.viajeros + item.viajeros;
+        memo2.estadiaAcum      = memo2.estadiaAcum + item.estadia;
+        memo2.estadiaCount      = memo2.estadiaCount + 1;
+        memo2.lat             = item.lat;
+        memo2.lon             = item.lon;
+
+        return memo2;
+      },{
+        "establecimientos" : 0,
+        "estadiaCount" : 0,
+        "estadiaAcum" : 0,
+        "habitaciones_disp" : 0,
+        "habitaciones_ocup" : 0,
+        "plazas_disp" : 0,
+        "plazas_ocup" : 0,
+        "viajeros" : 0,
+        "lat" : 0,
+        "lon" : 0
+      });
+
+      reduced.estadia = reduced.estadiaAcum/reduced.estadiaCount;
+
+
+
+      data.push(reduced);
+
+    });
+
+    return data;
+
+  }
+
+};
